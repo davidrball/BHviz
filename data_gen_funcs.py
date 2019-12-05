e7476c17cb962babd3f0a46723020e0a3601011a
David Ball <drb128@hotmail.com>
2019-12-05 09:48:49 -0700

    first commit on viz proj

---
+++ data_gen_funcs.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def get_vals(snapshot, x, y z): #get desired vals at specific positions from MHD snapshot
+
+
+    dens=0
+    temp=0
+    Bmag=0
+
+    return dens, temp, Bmag #
+
+
+
+def write_vals(valdict):
+
+    return 0
+    #write csv file
---
+++ data_process_funcs.py
@@ -0,0 +1,33 @@
+import numpy
+
+#here we are going to write functions that will take MHD data, generate the files we need as inputs to unity
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def split_dens(dens, x, y, z, deltax, deltay, deltaz):
+    #take density as an input, returns particles randomly spaced in cell (w/ size deltax, deltay, deltaz)
+
+
+
+    return 0
+
+
+
+def sparsify(prtlist, delete_frac):
+    #takes particle list and removes delete_frac of them
+
+    return 0 
